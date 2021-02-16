7f4cfc42dcc6fd9c441bcf23086de0bf3ebc646d
ZakiAhsan <79066165+ZakiGans@users.noreply.github.com>
2021-02-16 11:47:57 +0700

    Add files via upload

---
+++ database/menu/fun.js
@@ -0,0 +1,50 @@
+const fun = (prefix, botName, ownerName) => {
+        return `
+┏ *〈 ${botName} 〉*
+╿
+┷┯ *〈 BOT INFO 〉*
+   ╽
+   ┠≽ *Prefix* : 「  ${prefix}  」
+   ┠≽ *Creator* : ${ownerName}
+   ┠≽ *Version* : 0.0.5
+   ╿
+┯┷ *〈 ABOUT 〉*
+╽
+┠≽ *${prefix}info* (error)
+┃ *Desc* : Show Bot Details
+┠──────────────╼
+┠≽ *${prefix}blocklist*
+┃ *Desc* : Show Blocked User
+┠──────────────╼
+┠≽ *${prefix}chatlist* (error)
+┃ *Desc* : Show All Chat User
+┠──────────────╼
+┠≽ *${prefix}ping*
+┃ *Desc* : Show Connection Bot Speed
+┠──────────────╼
+┠≽ *${prefix}bugreport* <text>
+┃ *Desc* : Report Bug To Owner Bot
+╿
+┷┯ *〈 FUN 〉*
+   ╽
+   ┠≽ *${prefix}alay* <text>
+   ┃ *Desc* : Changing Words Into Alay
+   ┠──────────────╼
+   ┠≽ *${prefix}bucin*
+   ┃ *Desc* : Send a Random Slave of Love
+   ┠──────────────╼
+   ┠≽ *${prefix}trust*
+   ┃ *Desc* : Send a Random Truth
+   ┠──────────────╼
+   ┠≽ *${prefix}dare*
+   ┃ *Desc* : Send a Random Dare
+   ┠──────────────╼
+   ┠≽ *${prefix}simi* <text>
+   ┃ *Desc* : Talk to Simi
+   ┠──────────────╼
+   ┠≽ *${prefix}say* <text>
+   ┃ *Desc* : Saying:v
+   ╿ *${ownerName}*,
+   ╰╼≽ *Developer © ${botName}`
+}
+exports.fun = fun
